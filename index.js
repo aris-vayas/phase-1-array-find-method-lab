7a8f1a00bd33a62cff76940b9638ce87dae77746
Aris Vayas <aris.vayas@gmail.com>
2022-04-06 11:04:34 -0400

    commit tree 4f626351ec99a93cf93c3b6273ae1984373d8e00

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+]
+function finder(item){
+    return item.result ==="W"
+}
+function superbowlWin(thing){
+    if (thing.find(finder))
+    return thing.find(finder).year
+}
+
